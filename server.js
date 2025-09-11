d7b1a3f35179a07e879bfa42fc6c5a5bc92ab327
Aldo Quispe Gonzales <quispealdo1@gmail.com>
2025-09-11 03:41:37 -0500

    BackEnd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Swagger UI: http://localhost:${PORT}/api-docs`);
 });
